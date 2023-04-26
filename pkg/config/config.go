79365b92780d93b4e81c472965025be8149c909a
zd4rova <fakenotbad75@gmail.com>
2023-04-26 23:30:40 +0300

    update: config struct

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -13,7 +13,6 @@ type (
 		App  `yaml:"app"`
 		HTTP `yaml:"http"`
 		Log  `yaml:"logger"`
-		PG   `yaml:"postgres"`
 	}
 
 	App struct {
@@ -28,16 +27,6 @@ type (
 	Log struct {
 		Level      string `env-required:"true" yaml:"log_level"   env:"LOG_LEVEL"`
 		Structured bool   `yaml:"structured"   env:"STRUCTURED" env-default:"false"`
-	}
-
-	PG struct {
-		PostgresDB       string `env-required:"true"   env:"POSTGRES_DB"`
-		PostgresUser     string `env-required:"true"   env:"POSTGRES_USER"`
-		PostgresPassword string `env-required:"true"   env:"POSTGRES_PASSWORD"`
-		PostgresHost     string `env-required:"true"   env:"PG_HOST"`
-		MaxOpenConns     int    `yaml:"max_open_conns" env:"PG_MAX_OPEN_CONNS" env-default:"25"`
-		MaxIdleConns     int    `yaml:"max_idle_conns" env:"PG_MAX_IDLE_CONNS" env-default:"25"`
-		MaxIdleTime      string `yaml:"max_idle_time"  env:"PG_MAX_IDLE_TIME"  env-default:"15m"`
 	}
 )
 
